57c14d2aca5fef31f4e033d82991c3b1cbb2f878
Jawdan77 <34403286+Jawdan77@users.noreply.github.com>
2021-03-16 09:05:14 +0000

    Body

--- Template/loadTemplate.js
+++ Template/loadTemplate.js
@@ -16,30 +16,30 @@ $(function() {
     $(".tutorial").append($(".content"))
 
     /* LOW-KEY FORMATTING */
-    var totalHeaders = 0;
-    $("h2").each(function() {
-      if (totalHeaders > 0) {
-        $(this).append("<hr>");
-        var header = $(this);
-        $(".tutorialBookmarks").append("<a href=#Heading" + totalHeaders.toString() + ">" + header.text() + "</a>");
-        $(this).attr("id", "Heading" + totalHeaders.toString());
-      }
-      totalHeaders++;
-    });
-    $(".tutorialBookmarks").append("<hr>")
-
-    /* FORMATTING */
-    //PR.prettyPrint()
-    $(".gifPreview").hover(
-      function() {
-        var src = $(this).attr("src");
-        $(this).attr("src", src.substring(0, src.length - 4) + ".gif");
-        console.log("gif'd up");
-      },
-      function() {
-        var src = $(this).attr("src");
-        $(this).attr("src", src.substring(0, src.length - 4) + ".png");
-        console.log("png'd up");
-      }
-    );
+    //var totalHeaders = 0;
+    //$("h2").each(function() {
+    //  if (totalHeaders > 0) {
+    //    $(this).append("<hr>");
+    //    var header = $(this);
+    //    $(".tutorialBookmarks").append("<a href=#Heading" + totalHeaders.toString() + ">" + header.text() + "</a>");
+    //    $(this).attr("id", "Heading" + totalHeaders.toString());
+    //  }
+    //  totalHeaders++;
+    //});
+    //$(".tutorialBookmarks").append("<hr>")
+    //
+    ///* FORMATTING */
+    ////PR.prettyPrint()
+    //$(".gifPreview").hover(
+    //  function() {
+    //    var src = $(this).attr("src");
+    //    $(this).attr("src", src.substring(0, src.length - 4) + ".gif");
+    //    console.log("gif'd up");
+    //  },
+    //  function() {
+    //    var src = $(this).attr("src");
+    //    $(this).attr("src", src.substring(0, src.length - 4) + ".png");
+    //    console.log("png'd up");
+    //  }
+    //);
 });
