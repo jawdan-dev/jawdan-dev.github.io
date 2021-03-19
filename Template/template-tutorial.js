02c8d9f3454a4111c909ed1ede822d0a11e9f646
Jawdan77 <34403286+Jawdan77@users.noreply.github.com>
2021-03-19 16:16:21 +0000

    Template Fix

--- Template/template-tutorial.js
+++ Template/template-tutorial.js
@@ -1,14 +1,19 @@
 $.loadScript("https://jawdan77.github.io/Libraries/prettify.js");
 
 $(function() {
-    let bodyContent = $(document.body).contents();
-    $.loadHTML("https://jawdan77.github.io/Template/Raw-HTML/Body.html", function(data) {        
-        $(document.body).append(data);
-        $(".tutorial").append(bodyContent);          
-        $.loadHTML("https://jawdan77.github.io/Template/Raw-HTML/Head.html", function(data) { 
-            $(document.head).append(data);
-        });
+    $.loadHTML("https://jawdan77.github.io/Template/Raw-HTML/Style.html", function(data) { 
+        $(document.head).append(data);
     });
+
+    let bodyContent = $(document.body).contents();
+    $.loadHTML("https://jawdan77.github.io/Template/Raw-HTML/Base.html", function(data) {   
+        $.loadHTML("https://jawdan77.github.io/Template/Raw-HTML/Bookmark.html", function(data) { 
+            $(document.head).append(data);
+        });     
+        $(document.body).append(data);
+        $(".tutorial").append(bodyContent);    
+        
+    });      
 
     $.loadScript("https://jawdan77.github.io/Template/load-template.js");
 });
